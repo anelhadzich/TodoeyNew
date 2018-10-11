7c1eced8d513c5019b1ab204a35670a9fc5f3d81
Anel Hadzic <anelhadzich@gmail.com>
2018-10-11 14:45:50 +0200

    Persisting data using Codable protocol and Plist.

---
+++ TodoeyNew/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  TodoeyNew
+//
+//  Created by Anel Hadzic on 10/10/2018.
+//  Copyright © 2018 Anel Hadzic. All rights reserved.
+//
+
+import Foundation
+
+class Item : Codable{
+    
+    var title : String = ""
+    var value : Bool = false
+}
